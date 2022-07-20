2edc1db2bffc701b7d53753cc9c1a7d8d1e491a0
Your Name <you@example.com>
2022-07-20 20:39:35 +0900

    0720

---
+++ 20220719 과제.py
@@ -0,0 +1,42 @@
+'''
+# 특수문자 삭제하는법
+
+# 앞 뒤 문자 제거 strip -> 함수 사용
+
+1. 직접 키보드에서 특수 문자 다 찾는법
+2. 특수 문자 구글링
+'''
+
+# # 1 ~ number까지
+# number = int(input(''))
+
+# for i in range(1, number+1):
+#     print(i)
+
+# number ~ 0까지
+# number = int(input(''))
+
+# for i in range(number,-1,-1):
+#     print(i)
+
+
+# number = int(input(''))
+# result = 0
+
+# for num in range(1, number+1):
+#     if num % 2 == 1 :
+#         result += num
+#     else :
+#         continue
+    
+# print(result)
+
+
+equation_2 = {-b +- (b**2-4*a*c)**(1/2)} / 2*a
+
+a = 3
+b = 6
+c = - 5
+
+print(equation_2)
+
