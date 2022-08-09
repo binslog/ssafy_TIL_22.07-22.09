8575230acac8d7dc6711b855a07bccde7cc399bc
binslog <71168916+binslog@users.noreply.github.com>
2022-08-09 18:00:52 +0900

    Add files via upload

---
+++ dd.py
@@ -0,0 +1,10 @@
+num = list(map(str, input().split()))
+# num =[A, B, C]
+list = [0] * 17
+result =[]
+
+for i in range(len(list)) :
+    for j in range() :
+        list[j] = num[i]
+
+print(list)
---
+++ 연습.py
@@ -0,0 +1,24 @@
+
+
+# n = int(input())
+# char = list(map(str,input().split()))
+# result=[]
+#
+# for i in range(n):
+#     result += char[i]
+#
+# result = ''.join(result)
+#
+# print(result)
+#
+
+
+num = list(map(str, input().split())) # [A, B, C]
+list = [0] * 17 # [0],[0],[0],[0],[0],....[0]
+print(list)
+
+# for i in list :
+#     for j in range(7) :
+#         list[j] = num[i]
+#
+# print(list)
