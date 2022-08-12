ddbe3fef68b07d896ce72ae7980613c17c3211bb
bins95 <bins95@naver.com>
2022-08-12 17:59:31 +0900

    0812

---
+++ 민코딩 level 10.py
@@ -0,0 +1,9 @@
+# level 10
+# -------------------------------------------------
+array=list([0] * 3 for _ in range(3))
+print(array)
+n,m=3,3
+for i in range(n):
+    for j in range(m):
+        array[j][i]
+
