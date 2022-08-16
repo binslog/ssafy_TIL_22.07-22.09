652bd1be4b17ef3110f01b63ee0c327565c135c0
bins95 <bins95@naver.com>
2022-08-16 17:11:26 +0900

    0816

--- 연습.py
+++ 연습.py
@@ -1,33 +1,14 @@
+n = int(input())
+candidate = ["A","B","C","D"]
+path = [0] * n
 
-arr =[[4,7,1,8],[5,5,9,2],[5,9,5,9],[1,2,9,7]]
+def abc(level,candidate):
+    if level == n :
+        for i in range(level):
+            print(path[i],end='  ')
+    
+    for i in range(4):
+        path[level] = candidate[i]
+        abc(level+1,candidate)
 
-dx = [0,0,-1,1]
-dy = [1,-1,0,0]
-
-# location =[(i,j) for i in range(4) for j in range(4) if arr[i][j]==5]
-
-
-
-
-result = 0 # 결과 값 산출 
-
-for i in range(4):
-    for j in range(4):
-        
-        result += arr[x + dx[i]][y + dy[i]] # dx[0] 부터 이차원 배열에 접근!
-
-
-
-print(result)
-
-
-
-
-
-
-
-
-
-
-
-
+abc(0,candidate)
