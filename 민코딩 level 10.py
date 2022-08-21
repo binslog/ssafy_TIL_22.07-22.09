d3f9893fa2a49e0e85856d1a2cb86b4354e15629
Your Name <you@example.com>
2022-08-22 00:09:25 +0900

    0822

--- 민코딩 level 10.py
+++
@@ -1,9 +0,0 @@
-# level 10
-# -------------------------------------------------
-array=list([0] * 3 for _ in range(3))
-print(array)
-n,m=3,3
-for i in range(n):
-    for j in range(m):
-        array[j][i]
-
